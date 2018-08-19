0f9dc53347c4c8976f97e4d4485031d59fa73a01
Chris - Home <jcjabr@gmail.com>
2018-08-18 21:06:11 -0500

    experimenting w/ beautifulsoup4

--- Python-Library.py
+++ Python-Library.py
@@ -365,7 +365,79 @@ len(stores['BU'].unique())
 # clear workspace
 %reset
 
+
 #### to do:
 ## find all NaN/nulls in column
-## difference between dataframes (anti-join?). 
+## difference between dataframes (anti-join?).
 ## subset dataframe by id in another table
+
+
+
+
+
+# experimenting w/ beautifulsoup4 ###
+
+import requests
+
+page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+page
+page.status_code  # 200 means downloaded successfully, 4.* or 5.* means error
+
+page.content
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())  # print HTML content
+
+# finding 1 single line
+# find & number all children tags
+list(soup.children)  # soup.children is a list iterator
+[type(item) for item in list(soup.children)]
+html = list(soup.children)[2]
+list(html.children)
+for i, child in enumerate(list(html.children)):
+    print(i, child)
+
+body = list(html.children)[3]
+list(body.children)  # all content in index 1
+
+p = list(body.children)[1]
+p
+p.get_text()
+
+# finding all instances of a tag
+soup.find_all('p')  # returns a list
+soup.find_all('head')
+soup.find_all('title')
+
+soup.find_all('p')[0].get_text()  # use on single element of list
+soup.find('p')  # finds first instance
+
+### classes and ids ### # Find All Instances of a Tag at Once ###
+page = requests.get("http://dataquestio.github.io/web-scraping-pages/ids_and_classes.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+soup
+print(soup.prettify())
+
+# search all p(aragraph) tag w/ class
+soup.find_all('p', class_='outer-text')
+
+# search all class
+soup.find_all(class_="outer-text")  # same thing as above
+soup.find_all(class_="outer-text")
+
+# search by id
+soup.find_all(id="first")
+
+# Using CSS Selectors ###
+
+# find all p tags inside div
+soup.select("div p")  # returns list
+soup.select("body div p")  # body w/ child div w/ child p
+soup.select("p b")  # paragraph with bold text inside
+
+# reference: lambda one line function
+myfunc = lambda x, y: x + y
+myfunc(1, 2)  # 1 + 2
+
