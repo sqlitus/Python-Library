fc94f1526da2df93350417d24cb465047c8a9b9d
Chris <jcjabr@gmail.com>
2018-11-15 15:16:31 -0600

    * fully working headless script. using custom waitAndClick function. using file mgmt & checking for d/l completion. dynamic download path.

--- Scripts/Selenium - Call Data.py
+++ Scripts/Selenium - Call Data.py
@@ -19,10 +19,10 @@ fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.ms-
 
 # set headless
 opts = Options()
-# opts.headless = True
+opts.headless = True
 
 driver = webdriver.Firefox(firefox_profile=fp, options=opts)
-print("Firefox Headless Browser Invoked")
+print("**Firefox Headless Browser Invoked**")
 driver.get('https://10.2.89.122:8444/cuic/Main.htmx')   # .get() will automatically wait for page to load
 
 login = driver.find_element_by_id('rawUserName')
@@ -36,7 +36,7 @@ driver.find_element_by_id('j_password').send_keys(Keys.RETURN)
 
 # wait for navbar, then click reports
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.LINK_TEXT, 'Reports')))
-print("inside site. navigating to report.")
+print("logged in successfully. navigating to report.")
 reports_link = driver.find_elements_by_link_text('Reports')
 reports_link[0].click()
 
@@ -46,62 +46,55 @@ driver.switch_to.frame(driver.find_element_by_id("remote_iframe_4"))
 
 
 ### Xpath function
-# def waitAndClick(xpath_string, webdriver_object):
-#     WebDriverWait(webdriver_object, 20).until(EC.presence_of_element_located((By.XPATH, xpath_string)))
-#     webdriver_object.execute_script("arguments[0].click();", webdriver_object.find_element_by_xpath(xpath_string))
-#
-# waitAndClick("//span[@class='ellipsis ng-binding'][@title='Stock']", driver)
-# waitAndClick("//span[@class='ellipsis ng-binding'][@title='Unified CCX Historical']", driver)
-# waitAndClick("//span[@class='ellipsis ng-binding'][@title='Inbound']", driver)
-# waitAndClick("//span[@class='ellipsis ng-binding'][@title='Contact Service Queue Activity Report']", driver)
+def waitAndClick(xpath_string, webdriver_object):
+    WebDriverWait(webdriver_object, 20).until(EC.presence_of_element_located((By.XPATH, xpath_string)))
+    webdriver_object.execute_script("arguments[0].click();", webdriver_object.find_element_by_xpath(xpath_string))
+
 
 # Navigate to report: Stock > Unified CCX Historical > Inbound > Contact Service Queue Activity Report
-WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, "//span[@class='ellipsis ng-binding'][@title='Stock']"))) #.click()
-driver.execute_script("arguments[0].click();", driver.find_element_by_xpath("//span[@class='ellipsis ng-binding'][@title='Stock']"))
-driver.find_element_by_xpath("//span[@class='ellipsis ng-binding'][@title='Unified CCX Historical']").click()
-driver.find_element_by_xpath("//span[@class='ellipsis ng-binding'][@title='Inbound']").click()
-driver.find_element_by_xpath("//span[@class='ellipsis ng-binding'][@title='Contact Service Queue Activity Report']").click()
+waitAndClick("//span[@class='ellipsis ng-binding'][@title='Stock']", driver)
+waitAndClick("//span[@class='ellipsis ng-binding'][@title='Unified CCX Historical']", driver)
+waitAndClick("//span[@class='ellipsis ng-binding'][@title='Inbound']", driver)
+waitAndClick("//span[@class='ellipsis ng-binding'][@title='Contact Service Queue Activity Report']", driver)
 
 # Wait, Find date dropdown. Click. Wait & Select Yesterday.
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//a[@class='select-toggle form-control ng-binding ng-scope'][@title='Today']")))
+waitAndClick("//a[@class='select-toggle form-control ng-binding ng-scope'][@title='Today']", driver)
 print('selecting date parameters (yesterday).')
-dropdown_date = driver.find_element_by_xpath("//a[@class='select-toggle form-control ng-binding ng-scope'][@title='Today']")
-driver.execute_script("arguments[0].click();", dropdown_date)
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//a[@class='ng-binding'][@title='Yesterday']")))
-driver.execute_script("arguments[0].click();", driver.find_element_by_xpath("//a[@class='ng-binding'][@title='Yesterday']"))
+waitAndClick("//a[@class='ng-binding'][@title='Yesterday']", driver)
 
-# Choose parameters: search OPOS, Click CSQ Names (@param4) & add to filters
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@class='cuic-switcher-search-field'][@title='Search Available']")))
+# Choose parameters: search OPOS, then Click CSQ Names (@param4) & add to filters
+waitAndClick("//input[@class='cuic-switcher-search-field'][@title='Search Available']", driver)
 print('selecting team parameters.')
 driver.find_element_by_xpath("//input[@class='cuic-switcher-search-field'][@title='Search Available']").send_keys('opos')
 
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_Aloha']")))
-driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_Aloha']").click()
-driver.find_element_by_xpath("//div[@class='icon cuicfont right']").click()
-driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_NCR_Tech']").click()
-driver.find_element_by_xpath("//div[@class='icon cuicfont right']").click()
-driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_Payment']").click()
-driver.find_element_by_xpath("//div[@class='icon cuicfont right']").click()
-driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_R10']").click()
-driver.find_element_by_xpath("//div[@class='icon cuicfont right']").click()
+waitAndClick("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_Aloha']", driver)
+waitAndClick("//div[@class='icon cuicfont right']", driver)  # click right arrow
+waitAndClick("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_NCR_Tech']", driver)
+waitAndClick("//div[@class='icon cuicfont right']", driver)  # click right arrow
+waitAndClick("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_Payment']", driver)
+waitAndClick("//div[@class='icon cuicfont right']", driver)  # click right arrow
+waitAndClick("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_R10']", driver)
+waitAndClick("//div[@class='icon cuicfont right']", driver)  # click right arrow
 
 # Click 'Run' button
-driver.find_element_by_xpath("//button[@class='bc_lightgreen finishButton ng-binding']").click()
+waitAndClick("//button[@class='bc_lightgreen finishButton ng-binding']", driver)
 print('running report')
 
 # wait for table to load before exporting (helps for future navigation)
 WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, "//td[@class='progressTD'][contains(text(), 'Success')]")))
 
-
 # delete old file if exists
-# try:
-#     os.remove()
-#     print('deleting old CSQAR file before download')
-# except OSError:
-#     print('no duplicate file. downloading normally')
-#     pass
+CSQAR_filename = r"Contact Service Queue Activity Report-Contact Service Queue Activity Report.xls"
+CSQAR_full_path = os.path.join(os.getenv('USERPROFILE'), 'Downloads', CSQAR_filename)
 
-# Export to excel: Wait for page & visibility
+try:
+    os.remove(CSQAR_full_path)
+    print('deleting old CSQAR file before download')
+except OSError:
+    print('no duplicate file. downloading normally')
+    pass
+
+# Export table to excel: Wait for page & visibility
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@class='btn dropdown-toggle']")))
 dropdown_export = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//button[@class='btn dropdown-toggle']")))
 driver.execute_script("arguments[0].click();", dropdown_export)
@@ -109,34 +102,16 @@ WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//a
 driver.execute_script("arguments[0].click();", driver.find_element_by_xpath("//a[@class='ng-binding'][@title='Export']"))
 print('exporting file.')
 
+# wait & check if downloaded successfully
+for i in range(15):
+    if not os.path.exists(CSQAR_full_path):
+        time.sleep(1)
+        print('waiting on file download to complete.')
+    else:
+        print('download completed in', os.path.join(os.getenv('USERPROFILE'), 'Downloads'))
+        break
+else:
+    print('download not found after 15 seconds.')
+
 ### End
-time.sleep(3)
 driver.quit()
-
-
-
-
-### Reference
-
-## go back
-'''
-driver.execute_script("window.history.go(-1)")
-'''
-
-## action chain isn't holding click on everything
-'''
-ActionChains(driver).key_down(Keys.CONTROL) \
-    .click(driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_NCR_Tech']"))
-    .click(driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_Payment']"))
-    .click(driver.find_element_by_xpath("//span[@class='cuic-switcher-name ellipses ng-binding'][@title='OPOS_R10']"))
-    .key_up(Keys.CONTROL)
-    .perform()
-'''
-
-## custom wait & method function (unfinished)
-'''
-def wait_and_click(driver, xpath):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    ele = driver.find_element_by_xpath(xpath)
-    driver.execute_script("arguments[0].click();", ele)
-'''
